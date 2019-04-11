f7b0fdd07cb2711a0312825d3313e046b1f65c01
Cleverson Nahum <cleversonahum@gmail.com>
2019-04-11 09:55:40 -0300

    adaptando o header para o projeto

--- src/components/header.js
+++ src/components/header.js
@@ -9,7 +9,7 @@ function Header({ siteTitle }) {
     <nav className="bg-teal">
       <div className="flex flex-wrap items-center justify-between max-w-xl mx-auto p-4 md:p-8">
         <Link to="/" className="flex items-center no-underline text-white">
-          <svg
+          {/* <svg
             className="fill-current h-8 mr-2 w-8"
             width="54"
             height="54"
@@ -17,7 +17,7 @@ function Header({ siteTitle }) {
             xmlns="http://www.w3.org/2000/svg"
           >
             <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-          </svg>
+          </svg> */}
           <span className="font-bold text-xl tracking-tight">{siteTitle}</span>
         </Link>
 
@@ -45,21 +45,21 @@ function Header({ siteTitle }) {
               to="/"
               className="block mt-4 md:inline-block md:mt-0 mr-6 no-underline text-white"
             >
-              Home
+              Início
             </Link>
 
             <Link
               to="/about"
               className="block md:inline-block mt-4 md:mt-0 mr-6 no-underline text-white"
             >
-              About
+              Sobre
             </Link>
 
             <Link
               to="/contact"
               className="block md:inline-block mt-4 md:mt-0 no-underline text-white"
             >
-              Contact
+              Eventos
             </Link>
           </div>
         </div>

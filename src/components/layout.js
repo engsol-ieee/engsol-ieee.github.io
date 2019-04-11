144ef4173431f3dc95f17df0b41af4f257fa7897
Cleverson Nahum <cleversonahum@gmail.com>
2019-04-11 10:28:19 -0300

    mudando informações de rodapé

--- src/components/layout.js
+++ src/components/layout.js
@@ -29,16 +29,16 @@ function Layout({ children }) {
               <p className="text-white">
                 Created by{" "}
                 <a
-                  href="https://taylorbryant.blog"
+                  href=""
                   className="font-bold no-underline text-white"
                 >
-                  Taylor Bryant
+                  ComSoc - IEEE Belém
                 </a>
               </p>
 
               <p>
                 <a
-                  href="https://github.com/taylorbryant/gatsby-starter-tailwind"
+                  href="https://github.com/engsol-ieee"
                   className="font-bold no-underline text-white"
                 >
                   GitHub

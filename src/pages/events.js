fd28e603ebc23256771743d4ad669ce127387068
Cleverson Nahum <cleversonahum@gmail.com>
2019-05-14 15:21:53 -0300

    mudando a data do evento

--- src/pages/events.js
+++ src/pages/events.js
@@ -14,8 +14,8 @@ function ContactPage() {
       <div className="text-white">
       <h1>Próximos Eventos:</h1>
       <br /><br />
-      <h2>17/04/2019 - Git para Iniciantes: Conhecimentos básicos de Git para quem está começando</h2>
-      <p>Minicurso sobre a ferramenta Git, amplamente utilizada para versionamento de código de projetos. O conhecimento de Git é essencial para todos que trabalham na área de tecnologia por ser a ferramenta mais utilizada para gerenciamento de projetos.<br /><b>O minicurso é gratuito e pedimos a doação de alimentos não perecíveis.</b><br /><b>Quantidade de vagas</b>:  18 (para os 18 primeiros inscritos)<br />Inscrições no <a href="https://www.sympla.com.br/curso-de-git-para-iniciantes__526100">Link</a></p>
+      <h2>24/05/2019 - Git para Iniciantes: Conhecimentos básicos de Git para quem está começando</h2>
+      <p>Minicurso sobre a ferramenta Git, amplamente utilizada para versionamento de código de projetos. O conhecimento de Git é essencial para todos que trabalham na área de tecnologia por ser a ferramenta mais utilizada para gerenciamento de projetos.<br /><b>O minicurso é gratuito e pedimos a doação de alimentos não perecíveis.</b><br /><b>Quantidade de vagas</b>:  18 (para os primeiros inscritos)<br />Inscrições no <a href="https://www.sympla.com.br/curso-de-git-para-iniciantes__526100">Link</a></p>
       </div>
     </Layout>
     </nav>
